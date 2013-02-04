4bab537819caa4c604b37dcf58c327b8190c02d9
Chris Dombroski <cdombroski@icanttype.org>
2013-02-04 07:21:01 -0500

    Import into github

---
+++ BankOCR/src/bankocr/core.clj
@@ -0,0 +1,57 @@
+(ns bankocr.core)
+
+(def digits
+  {0
+   [" _ "
+    "| |"
+    "|_|"]
+   1
+   ["   "
+    "  |"
+    "  |"]
+   2
+   [" _ "
+    " _|"
+    "|_ "]
+   3
+   [" _ "
+    " _|"
+    " _|"]
+   4
+   ["   "
+    "|_|"
+    "  |"]
+   5
+   [" _ "
+    "|_ "
+    " _|"]
+   6
+   [" _ "
+    "|_ "
+    "|_|"]
+   7
+   [" _ "
+    "  |"
+    "  |"]
+   8
+   [" _ "
+    "|_|"
+    "|_|"]
+   9
+   [" _ "
+    "|_|"
+    " _|"]})
+
+(defn lines2digits [line1 line2 line3]
+  (for [x (range 9)]
+    [(.substring line1 (* x 3) (+ (* x 3) 3))
+     (.substring line2 (* x 3) (+ (* x 3) 3))
+     (.substring line3 (* x 3) (+ (* x 3) 3))]))
+
+(defn digit2number [digit]
+  (some #(when (= (val %) digit) (key %)) digits))
+
+(defn read-account-number [instream]
+  (binding [*in* instream]
+    (map digit2number (lines2digits (read-line) (read-line) (read-line)))))
+
