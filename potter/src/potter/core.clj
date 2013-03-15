c91bbcc01a3d9791fa3a5b501a8a9141c11d0dea
cdombroski <cdombroski@icanttype.org>
2013-03-15 16:05:24 -0400

    formatting

--- potter/src/potter/core.clj
+++ potter/src/potter/core.clj
@@ -15,20 +15,22 @@
 
 (defn- group-books [books]
   (let [book-frequencies (into {0 0, 1 0, 2 0, 3 0, 4 0} (frequencies books))
-        init-groups (loop [book-freqs book-frequencies
-                           result {}
-                           group 4]
-          (let [group-num (if (> (count (remove zero? (vals book-freqs))) group)
-                            (reduce min (remove zero? (vals book-freqs)))
-                            0)
+        init-groups
+        (loop [book-freqs book-frequencies
+               result {}
+               group 4]
+          (let [group-num
+                (if (> (count (remove zero? (vals book-freqs))) group)
+                  (reduce min (remove zero? (vals book-freqs)))
+                  0)
                 new-freqs (for [[k v] book-freqs]
-                            {k (max 0 (- v group-num))})]
+              {k (max 0 (- v group-num))})]
             (if (pos? group)
               (recur (apply merge new-freqs) (conj result {group group-num}) (dec group))
               (conj result {group group-num}))))
         pairs-3-5 (min (get init-groups 4) (get init-groups 2))] ;paired groups of 3 and 5 are better as pairs of 4
-      (for [[k v] init-groups]
-        (case k
-          (2 4) {k (- v pairs-3-5)}
-          3 {k (+ v (* 2 pairs-3-5))}
-          {k v}))))
+    (for [[k v] init-groups]
+      (case k
+        (2 4) {k (- v pairs-3-5)}
+        3 {k (+ v (* 2 pairs-3-5))}
+        {k v}))))
